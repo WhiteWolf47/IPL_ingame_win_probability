5f05986258c4af309759fc0d0f191b0a0e64bf97
ANURAG BHANDARI <bhandari2003anurag@gmail.com>
2022-05-07 10:51:33 +0530

    handled edge case

--- app.py
+++ app.py
@@ -64,31 +64,35 @@ with col7:
     wickets = st.number_input('Wickets out')
 
 if st.button('Predict Probability'):
-    runs_left = target - score
-    balls_left = 120 - (overs*6)
-    crr = score/overs
-    rrr = (runs_left*6)/balls_left
+    if overs == 0:
+        st.write('**OVERS CANNOT BE ZERO**')
 
-    bowl_city = team_city[bowling_team]
-    bat_city = team_city[batting_team]
-    home_team = 0
-
-    if selected_city == bowl_city:
-        home_team = 0
-    elif selected_city == bat_city:
-        home_team = 1
     else:
-        home_team = 2
+        runs_left = target - score
+        balls_left = 120 - (overs*6)
+        crr = score/overs
+        rrr = (runs_left*6)/balls_left
 
-    bowl_hwp  = hwp[bowling_team]
-    bat_hwp = hwp[batting_team]
-    bowl_awp = awp[bowling_team]
-    bat_awp = awp[batting_team]
+        bowl_city = team_city[bowling_team]
+        bat_city = team_city[batting_team]
+        home_team = 0
 
-    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'toss_winner':[toss_winner],'toss_decision':[toss_decision],'runs_left':[runs_left],'balls_left':[balls_left],'fallen_wickets':[wickets],'Target':[target],'crr':[crr],'rrr':[rrr], 'Home_team':[home_team], 'bowl_hwp':[bowl_hwp], 'bat_hwp':[bat_hwp], 'bowl_awp':[bowl_awp], 'bat_awp':[bat_awp]})
+        if selected_city == bowl_city:
+            home_team = 0
+        elif selected_city == bat_city:
+            home_team = 1
+        else:
+            home_team = 2
 
-    result = pipe.predict_proba(input_df)
-    loss = result[0][0]
-    win = result[0][1]
-    st.header(batting_team + "- " + str(round(win*100)) + "%")
-    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+        bowl_hwp  = hwp[bowling_team]
+        bat_hwp = hwp[batting_team]
+        bowl_awp = awp[bowling_team]
+        bat_awp = awp[batting_team]
+
+        input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'toss_winner':[toss_winner],'toss_decision':[toss_decision],'runs_left':[runs_left],'balls_left':[balls_left],'fallen_wickets':[wickets],'Target':[target],'crr':[crr],'rrr':[rrr], 'Home_team':[home_team], 'bowl_hwp':[bowl_hwp], 'bat_hwp':[bat_hwp], 'bowl_awp':[bowl_awp], 'bat_awp':[bat_awp]})
+
+        result = pipe.predict_proba(input_df)
+        loss = result[0][0]
+        win = result[0][1]
+        st.header(batting_team + "- " + str(round(win*100)) + "%")
+        st.header(bowling_team + "- " + str(round(loss*100)) + "%")
